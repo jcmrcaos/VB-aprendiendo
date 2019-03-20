8a2c270c322377ed78b76865629a71d62676c4bd
Juan Carlos Caballero Padrino <45860986+jcmrcaos@users.noreply.github.com>
2019-03-20 12:14:22 +0100

    Update Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -2,7 +2,7 @@
 
     Sub Main()
         Dim numero As Integer
-        numero = 5
+        numero = 6
         If numero > 3 Then Console.WriteLine("El numero es mayor que 3")
         Console.ReadLine()
     End Sub
