3733ab4e4d0ce58dde0363ee13fe11f825ee9696
Juan Carlos Caballero Padrino <45860986+jcmrcaos@users.noreply.github.com>
2019-03-20 12:13:46 +0100

    inicio repositorio
    inicio repositorio

---
+++ Module1.vb
@@ -0,0 +1,10 @@
+﻿Module condicionalIf
+
+    Sub Main()
+        Dim numero As Integer
+        numero = 5
+        If numero > 3 Then Console.WriteLine("El numero es mayor que 3")
+        Console.ReadLine()
+    End Sub
+
+End Module
